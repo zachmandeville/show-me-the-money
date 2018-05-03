85854d7ed3e0c09c3405a068af5f5a44d2e12645
Zach <ssb.pub.key.is@ZqH7Mctu/7DNInxuwl12ECjfrAKUX2tBLq1rOldNhg0=.ed25519>
2018-05-04 11:52:13 +1200

    add timer component

---
+++ client/components/Timer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+
+var Timer = () => {
+  return (
+    <div>
+      <h1>Hi, this is Timer</h1>
+    </div>
+  )
+}
+
+export default Timer
+
