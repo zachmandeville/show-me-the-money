432f0395e471b603bb1989e0f8a058e4eb574115
Rosie Burkett <burkettroz@gmail.com>
2018-05-04 09:51:24 +1200

    fixed seed data meetings

--- seeds/populateMeetings.js
+++ seeds/populateMeetings.js
@@ -9,23 +9,23 @@ exports.seed = function(knex, Promise) {
             scheduled_date: new Date(),
             start_time: 800,
             end_time: 900,
-            attendees: [
+            attendees: JSON.stringify([
               {id: 1, user_name: "bob", first_name: "Bob", last_name: "Smith", hourly_wage: 10},
               {id: 2, user_name: "alice", first_name: "Alice", last_name: "Smith", hourly_wage: 10},
               {id: 3, user_name: "eve", first_name: "Eve", last_name: "Miller", hourly_wage: 10},
               {id: 4, user_name: "chris", first_name: "Chris", last_name: "Knight", hourly_wage: 10}
-              ]
+              ])
           },
           {
             name: "Followup on API",
             scheduled_date: new Date(),
             start_time: 800,
             end_time: 900,
-            attendees: [
+            attendees:JSON.stringify( [
               {id: 1, user_name: "bob", first_name: "Bob", last_name: "Smith", hourly_wage: 10},
               {id: 2, user_name: "alice", first_name: "Alice", last_name: "Smith", hourly_wage: 10},
               {id: 3, user_name: "eve", first_name: "Eve", last_name: "Miller", hourly_wage: 10}
-              ]
+              ])
           }
         ]);
     });
