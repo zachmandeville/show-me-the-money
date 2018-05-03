7b606354c110e18741acf6fd0424d625895b2f83
Annika <liebste.anni@gmail.com>
2018-05-03 16:51:34 +1200

    Meeting screen styling added

--- client/components/Meeting.jsx
+++ client/components/Meeting.jsx
@@ -3,7 +3,21 @@ import {connect} from 'react-redux'
 
 function Meeting (props) {
   return <div className="container">
+  <br/>
     <h2 className="title is-2">Start Meeting</h2>
+      <div className="field has-addons">
+        <div className="control" id="plus-button">
+            <a className="button is-primary">
+              +
+            </a>
+          </div>
+          <div className="control" id="name-entry">
+            <input className="input" type="text" placeholder="Name"/>
+          </div>
+          <div className="control" id="cost-entry">
+            <input className="input" type="text" placeholder="$"/>
+          </div>
+      </div>
   </div>
 }
 
