a055532e60ea2500d3c636bf08e15ff351958851
Rosie Burkett <burkettroz@gmail.com>
2018-05-04 14:25:36 +1200

    draft moneycalc version 1

--- client/components/Counter.jsx
+++ client/components/Counter.jsx
@@ -1,16 +1,47 @@
 import React from 'react'
 import {HashRouter as Router, Route, Link} from 'react-router-dom'
 import {connect} from 'react-redux'
+import {getMeetingAttendees} from '../actions/meetings'
+import {newTimeAction} from '../actions/time' // change to whatever Laura and Zach call this
 
-function Counter (props) {
+
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: this.props.time
+        }
+        this.calcMoney = this.calcMoney.bind(this)
+    }
+
+calcMoney() {
+  var totalSalary = 0
+  var costPerSecond = 0
+    for (let i = 0; i < attendees.length; i++) {
+    totalSalary += attendees[i].hourly_wage
+  }
+  return costPerSecond = ((totalSalary / 3600) * this.state.time) 
+
+}
+
+render() {
     return <div className="container">
     <div className="box">
-        Cost counter
+        Cost counter:
+       <p> {this.calcMoney()} </p>
     </div>
     <div className="box">
         Timer
     </div>
     </div>
   }
+}
   
-  export default connect()(Counter)
+  function mapStateToProps(state){
+    return {
+        attendees: state.attendees,
+        time: state.time
+    }
+  }
+  export default connect(mapStateToProps)(Counter)
---
+++ client/reducers/money.js
@@ -0,0 +1,8 @@
+function money (state = [], action){
+    switch (action.type) {
+        case NEW_TIME: 
+            return action.money
+    }
+}
+
+export default money
